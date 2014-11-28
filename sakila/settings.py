e09aa994b1dafd4f183629c6487e42452c7e4a2b
Abdulla AlKhenji <al.khenji@gmail.com>
2014-11-28 22:05:45 +0300

    fixed static

--- sakila/settings.py
+++ sakila/settings.py
@@ -80,7 +80,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
-STATIC_URL = '/static/'
+# STATIC_URL = '/static/'
 
 # Parse database configuration from $DATABASE_URL
 import dj_database_url
@@ -96,8 +96,8 @@ ALLOWED_HOSTS = ['*']
 import os
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = 'staticfiles'
-STATIC_URL = '/sakila_ok/static/'
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'sakila_ok/static'),
 )
